54c9750cca7df49e82c0c2a29801199ae51ebc5a
damon <zhaorong@outlook.com>
2023-08-22 23:43:49 +0800

    增加自定义异常

---
+++ Woa.Common/Exceptions/BadGatewayException.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+
+namespace Woa.Common;
+
+/// <summary>
+///    502 Bad Gateway
+/// </summary>
+public class BadGatewayException : Exception
+{
+	public BadGatewayException()
+	{
+	}
+
+	protected BadGatewayException(SerializationInfo info, StreamingContext context)
+		: base(info, context)
+	{
+	}
+
+	public BadGatewayException(string message)
+		: base(message)
+	{
+	}
+
+	public BadGatewayException(string message, Exception innerException)
+		: base(message, innerException)
+	{
+	}
+}
