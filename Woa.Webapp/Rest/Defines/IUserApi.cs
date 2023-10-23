b19d10305a86db272e02d3a0c7ff4be46c4818ef
damon <zhaorong@outlook.com>
2023-10-23 21:10:01 +0800

    增加IUserApi、IRoleApi

--- Woa.Webapp/Rest/Defines/IUserApi.cs
+++ Woa.Webapp/Rest/Defines/IUserApi.cs
@@ -1,6 +1,16 @@
-﻿namespace Woa.Webapp.Rest;
+﻿using Refit;
+using Woa.Transit;
+
+namespace Woa.Webapp.Rest;
 
 internal interface IUserApi
 {
-	
+	[Get("/api/user")]
+	Task<IApiResponse<List<UserItemDto>>> SearchAsync([Query] UserQueryDto condition, [Query] int page, [Query] int size, CancellationToken cancellationToken = default);
+
+	[Get("/api/user/{id}")]
+	Task<IApiResponse<UserDetailDto>> GetAsync(int id, CancellationToken cancellationToken = default);
+
+	[Post("/api/user")]
+	Task<IApiResponse> CreateAsync([Body] UserCreateDto model, CancellationToken cancellationToken = default);
 }
