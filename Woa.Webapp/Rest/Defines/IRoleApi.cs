88c5c2be1bf5c51f6fcab68b7a6cdbb5e5e6040c
damon <zhaorong@outlook.com>
2023-10-26 11:15:55 +0800

    接口调整

--- Woa.Webapp/Rest/Defines/IRoleApi.cs
+++ Woa.Webapp/Rest/Defines/IRoleApi.cs
@@ -5,8 +5,11 @@ namespace Woa.Webapp.Rest;
 
 public interface IRoleApi
 {
-	[Get("/api/role")]
+	[Get("/api/role/list")]
 	Task<IApiResponse<List<RoleInfoDto>>> SearchAsync([Query] RoleQueryDto condition, [Query] int page, [Query] int size, CancellationToken cancellationToken = default);
+
+	[Get("/api/role/count")]
+	Task<IApiResponse<int>> CountAsync([Query] RoleQueryDto condition, CancellationToken cancellationToken = default);
 
 	[Get("/api/role/{id}")]
 	Task<IApiResponse<RoleInfoDto>> GetAsync(int id, CancellationToken cancellationToken = default);
--- Woa.Webapp/Rest/Defines/IUserApi.cs
+++ Woa.Webapp/Rest/Defines/IUserApi.cs
@@ -5,12 +5,21 @@ namespace Woa.Webapp.Rest;
 
 internal interface IUserApi
 {
-	[Get("/api/user")]
+	[Get("/api/user/list")]
 	Task<IApiResponse<List<UserItemDto>>> SearchAsync([Query] UserQueryDto condition, [Query] int page, [Query] int size, CancellationToken cancellationToken = default);
+
+	[Get("/api/user/count")]
+	Task<IApiResponse<int>> CountAsync([Query] UserQueryDto condition, CancellationToken cancellationToken = default);
 
 	[Get("/api/user/{id}")]
 	Task<IApiResponse<UserDetailDto>> GetAsync(int id, CancellationToken cancellationToken = default);
 
 	[Post("/api/user")]
 	Task<IApiResponse> CreateAsync([Body] UserCreateDto model, CancellationToken cancellationToken = default);
+
+	[Put("/api/user/{id}")]
+	Task<IApiResponse> UpdateAsync(int id, [Body] UserUpdateDto model, CancellationToken cancellationToken = default);
+
+	[Put("/api/user/{id}/role")]
+	Task<IApiResponse> UpdateRoleAsync(int id, [Body] List<int> model, CancellationToken cancellationToken = default);
 }
